189382bb0a616844840fc225e57f29e2f210f4f9
Alyson <emerald.aly@gmail.com>
2017-07-10 13:19:59 -0400

    code to review

--- server.js
+++ server.js
@@ -7,17 +7,16 @@ const logger= require('morgan');
 
 const PORT = process.env.PORT || 8000;
 
-
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
-
 app.use(logger('dev'));
 app.use(express.static(__dirname + "/public"));
 app.use(bodyParser.urlencoded({
   extended: false
 }));
 app.use(bodyParser.json());
+
+app.get('/', function (req, res) {
+  res.send('Hello World!')
+})
 
 module.exports = app.listen(PORT, function(){
   console.log("listening on PORT:" + PORT);
